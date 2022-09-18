740a8d193056df03a9a966ac5f7b9ad6c5e355a4
Valeriy <cherkasov77@mail.ru>
2022-09-18 15:52:54 +0400

    Algoritmexe

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string message = "hel";
+ Console.Write($"'{message}'");
+string message1 = "hello";
+Console.Write($"'{message1}'");
+string message2 = "h";
+Console.Write($"'{message2}'");
+// получаем символ
+//char firstChar = message[1]; // символ 'e'
+//Console.WriteLine(firstChar);   //e
+if (message.Length <= 3|| message1.Length<=3||message2.Length<=3)
+{
+//message.Length <= 3;
+
+Console.WriteLine();
+    Console.WriteLine($"{message.Length <= 3},{message1.Length<=3},{message2.Length<=3} ");  // длина строки  
+  
+}
